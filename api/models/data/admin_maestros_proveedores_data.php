027fe338d62609ebfe6b5af67f8142aa64b9f5d9
RodrigoDiaz13 <108245842+RodrigoDiaz13@users.noreply.github.com>
2024-06-29 15:38:59 -0600

    data listo

---
+++ api/models/data/admin_maestros_proveedores_data.php
@@ -0,0 +1,57 @@
+<?php
+// Se incluye la clase para validar los datos de entrada.
+require_once('../../helpers/validator.php');
+// Se incluye la clase padre.
+require_once('../../models/handler/admin_maestros_proveedores_handler.php');
+/*
+ *	Clase para manejar el encapsulamiento de los datos de la tabla PRODUCTO.
+ */
+class ProveedorData extends ProveedorHandler
+{
+    /*
+     *  Atributos adicionales.
+     */
+    private $data_error = null;
+    private $filename = null;
+
+    /*
+     *   Métodos para validar y establecer los datos.
+     */
+    public function setId($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id_proveedor = $value;
+            return true;
+        } else {
+            $this->data_error = 'El identificador del proveedor es incorrecto';
+            return false;
+        }
+    }
+
+    public function setNombre($value, $min = 2, $max = 50)
+    {
+        if (!Validator::validateAlphanumeric($value)) {
+            $this->data_error = 'El nombre debe ser un valor alfanumérico';
+            return false;
+        } elseif (Validator::validateLength($value, $min, $max)) {
+            $this->nombre_proveedor = $value;
+            return true;
+        } else {
+            $this->data_error = 'El nombre debe tener una longitud entre ' . $min . ' y ' . $max;
+            return false;
+        }
+    }
+
+    /*
+     *  Métodos para obtener el valor de los atributos adicionales.
+     */
+    public function getDataError()
+    {
+        return $this->data_error;
+    }
+
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+}
