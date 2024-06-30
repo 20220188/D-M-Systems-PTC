2c3984fa6f76e2185cd35d8aa7278cb1e1fba795
RodrigoDiaz13 <108245842+RodrigoDiaz13@users.noreply.github.com>
2024-06-29 20:24:35 -0600

    haciendo crud

--- api/services/admin/admin_maestros_proveedores.php
+++ api/services/admin/admin_maestros_proveedores.php
@@ -11,7 +11,7 @@ if (isset($_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'session' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null, 'username' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    if (isset($_SESSION['idAdministrador'])) {
+    if (isset($_SESSION['id_proveedor'])) {
         $result['session'] = 1;
         // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
         switch ($_GET['action']) {
