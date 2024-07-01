b65920f9f9f4905fdc62bea07bc90237d120e704
RodrigoDiaz13 <108245842+RodrigoDiaz13@users.noreply.github.com>
2024-06-30 21:21:54 -0600

    Crud de usuarios

--- api/models/handler/admin_usuario_handler.php
+++ api/models/handler/admin_usuario_handler.php
@@ -1,82 +1,66 @@
 <?php
 // Se incluye la clase para trabajar con la base de datos.
 require_once('../../helpers/database.php');
+
 /*
-*	Clase para manejar el comportamiento de los datos de la tabla proveedor.
+*	Clase para manejar el comportamiento de los datos de la tabla tb_usuarios.
 */
-class ProveedorHandler
+class UsuarioHandler
 {
     /*
     *   Declaración de atributos para el manejo de datos.
     */
-    protected $id_proveedor = null;
-    protected $codigo_proveedor = null;
-    protected $nombre_proveedor = null;
-    protected $pais_proveedor = null;
-    protected $giro_negocio_proveedor = null;
-    protected $dui_proveedor = null;
-    protected $nombre_comercial_proveedor = null;
-    protected $fecha_proveedor = null;
-    protected $nit_proveedor = null;
-    protected $telefono_proveedor = null;
-    protected $contacto_proveedor = null;
-    protected $direccion_proveedor = null;
-    protected $departamento_proveedor = null;
-    protected $municipio_proveedor = null;
+    protected $id_usuario = null;
+    protected $usuario = null;
+    protected $clave = null;
+    protected $correo = null;
+    protected $nombre = null;
+    protected $DUI = null;
+    protected $telefono = null;
+    protected $id_nivel_usuario = null;
 
     /*
-    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
+    *   Métodos para realizar las operaciones CRUD (create, read, update, and delete).
     */
-    public function searchRows($value)
-    {
-        $value = '%' . $value . '%';
-        $sql = 'SELECT id_proveedor, codigo_proveedor, nombre_proveedor, pais_proveedor, giro_negocio_proveedor, dui_proveedor, nombre_comercial_proveedor, fecha_proveedor, nit_proveedor, telefono_proveedor, contacto_proveedor, direccion_proveedor, departamento_proveedor, municipio_proveedor
-                FROM tb_proveedores
-                WHERE nombre_proveedor LIKE ? OR codigo_proveedor LIKE ?
-                ORDER BY nombre_proveedor';
-        $params = array($value, $value);
-        return Database::getRows($sql, $params);
-    }
-
     public function createRow()
     {
-        $sql = 'INSERT INTO tb_proveedores (codigo_proveedor, nombre_proveedor, pais_proveedor, giro_negocio_proveedor, dui_proveedor, nombre_comercial_proveedor, fecha_proveedor, nit_proveedor, telefono_proveedor, contacto_proveedor, direccion_proveedor, departamento_proveedor, municipio_proveedor)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        $params = array($this->codigo_proveedor, $this->nombre_proveedor, $this->pais_proveedor, $this->giro_negocio_proveedor, $this->dui_proveedor, $this->nombre_comercial_proveedor, $this->fecha_proveedor, $this->nit_proveedor, $this->telefono_proveedor, $this->contacto_proveedor, $this->direccion_proveedor, $this->departamento_proveedor, $this->municipio_proveedor);
+        $sql = 'INSERT INTO tb_usuarios (usuario, clave, correo, nombre, DUI, telefono, id_nivel_usuario)
+                VALUES (?, ?, ?, ?, ?, ?, ?)';
+        $params = array($this->usuario, $this->clave, $this->correo, $this->nombre, $this->DUI, $this->telefono, $this->id_nivel_usuario);
         return Database::executeRow($sql, $params);
     }
 
     public function readAll()
     {
-        $sql = 'SELECT id_proveedor, codigo_proveedor, nombre_proveedor, pais_proveedor, giro_negocio_proveedor, dui_proveedor, nombre_comercial_proveedor, fecha_proveedor, nit_proveedor, telefono_proveedor, contacto_proveedor, direccion_proveedor, departamento_proveedor, municipio_proveedor
-                FROM tb_proveedores
-                ORDER BY nombre_proveedor';
+        $sql = 'SELECT id_usuario, usuario, clave, correo, nombre, DUI, telefono, id_nivel_usuario
+                FROM tb_usuarios
+                ORDER BY nombre';
         return Database::getRows($sql);
     }
 
     public function readOne()
     {
-        $sql = 'SELECT id_proveedor, codigo_proveedor, nombre_proveedor, pais_proveedor, giro_negocio_proveedor, dui_proveedor, nombre_comercial_proveedor, fecha_proveedor, nit_proveedor, telefono_proveedor, contacto_proveedor, direccion_proveedor, departamento_proveedor, municipio_proveedor
-                FROM tb_proveedores
-                WHERE id_proveedor = ?';
-        $params = array($this->id_proveedor);
+        $sql = 'SELECT id_usuario, usuario, clave, correo, nombre, DUI, telefono, id_nivel_usuario
+                FROM tb_usuarios
+                WHERE id_usuario = ?';
+        $params = array($this->id_usuario);
         return Database::getRow($sql, $params);
     }
 
     public function updateRow()
     {
-        $sql = 'UPDATE tb_proveedores
-                SET codigo_proveedor = ?, nombre_proveedor = ?, pais_proveedor = ?, giro_negocio_proveedor = ?, dui_proveedor = ?, nombre_comercial_proveedor = ?, fecha_proveedor = ?, nit_proveedor = ?, telefono_proveedor = ?, contacto_proveedor = ?, direccion_proveedor = ?, departamento_proveedor = ?, municipio_proveedor = ?
-                WHERE id_proveedor = ?';
-        $params = array($this->codigo_proveedor, $this->nombre_proveedor, $this->pais_proveedor, $this->giro_negocio_proveedor, $this->dui_proveedor, $this->nombre_comercial_proveedor, $this->fecha_proveedor, $this->nit_proveedor, $this->telefono_proveedor, $this->contacto_proveedor, $this->direccion_proveedor, $this->departamento_proveedor, $this->municipio_proveedor, $this->id_proveedor);
+        $sql = 'UPDATE tb_usuarios
+                SET usuario = ?, clave = ?, correo = ?, nombre = ?, DUI = ?, telefono = ?, id_nivel_usuario = ?
+                WHERE id_usuario = ?';
+        $params = array($this->usuario, $this->clave, $this->correo, $this->nombre, $this->DUI, $this->telefono, $this->id_nivel_usuario, $this->id_usuario);
         return Database::executeRow($sql, $params);
     }
 
     public function deleteRow()
     {
-        $sql = 'DELETE FROM tb_proveedores
-                WHERE id_proveedor = ?';
-        $params = array($this->id_proveedor);
+        $sql = 'DELETE FROM tb_usuarios
+                WHERE id_usuario = ?';
+        $params = array($this->id_usuario);
         return Database::executeRow($sql, $params);
     }
 }
---
+++ api/services/admin/admin_usuarios.php
@@ -0,0 +1,108 @@
+<?php
+// Se incluye la clase del modelo.
+require_once('../../models/data/usuario_data.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $usuario = new UsuarioData;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null);
+    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
+    if (isset($_SESSION['idAdministrador'])) {
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'searchRows':
+                if (!Validator::validateSearch($_POST['search'])) {
+                    $result['error'] = Validator::getSearchError();
+                } elseif ($result['dataset'] = $usuario->searchRows($_POST['search'])) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen ' . count($result['dataset']) . ' coincidencias';
+                } else {
+                    $result['error'] = 'No hay coincidencias';
+                }
+                break;
+            case 'createRow':
+                $_POST = Validator::validateForm($_POST);
+                if (
+                    !$usuario->setUsuario($_POST['usuario']) or
+                    !$usuario->setClave($_POST['clave']) or
+                    !$usuario->setCorreo($_POST['correo']) or
+                    !$usuario->setNombre($_POST['nombre']) or
+                    !$usuario->setDUI($_POST['DUI']) or
+                    !$usuario->setTelefono($_POST['telefono']) or
+                    !$usuario->setIdNivelUsuario($_POST['id_nivel_usuario'])
+                ) {
+                    $result['error'] = $usuario->getDataError();
+                } elseif ($usuario->createRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Usuario creado correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            case 'readAll':
+                if ($result['dataset'] = $usuario->readAll()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen ' . count($result['dataset']) . ' registros';
+                } else {
+                    $result['error'] = 'No existen usuarios registrados';
+                }
+                break;
+            case 'readOne':
+                if (!$usuario->setIdUsuario($_POST['id_usuario'])) {
+                    $result['error'] = $usuario->getDataError();
+                } elseif ($result['dataset'] = $usuario->readOne()) {
+                    $result['status'] = 1;
+                } else {
+                    $result['error'] = 'Usuario inexistente';
+                }
+                break;
+            case 'updateRow':
+                $_POST = Validator::validateForm($_POST);
+                if (
+                    !$usuario->setIdUsuario($_POST['id_usuario']) or
+                    !$usuario->setUsuario($_POST['usuario']) or
+                    !$usuario->setClave($_POST['clave']) or
+                    !$usuario->setCorreo($_POST['correo']) or
+                    !$usuario->setNombre($_POST['nombre']) or
+                    !$usuario->setDUI($_POST['DUI']) or
+                    !$usuario->setTelefono($_POST['telefono']) or
+                    !$usuario->setIdNivelUsuario($_POST['id_nivel_usuario'])
+                ) {
+                    $result['error'] = $usuario->getDataError();
+                } elseif ($usuario->updateRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Usuario modificado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al modificar el usuario';
+                }
+                break;
+            case 'deleteRow':
+                if (!$usuario->setIdUsuario($_POST['id_usuario'])) {
+                    $result['error'] = $usuario->getDataError();
+                } elseif ($usuario->deleteRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Usuario eliminado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al eliminar el usuario';
+                }
+                break;
+            default:
+                $result['error'] = 'Acción no disponible dentro de la sesión';
+        }
+        // Se obtiene la excepción del servidor de base de datos por si ocurrió un problema.
+        $result['exception'] = Database::getException();
+        // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+        header('Content-type: application/json; charset=utf-8');
+        // Se imprime el resultado en formato JSON y se retorna al controlador.
+        print(json_encode($result));
+    } else {
+        print(json_encode('Acceso denegado'));
+    }
+} else {
+    print(json_encode('Recurso no disponible'));
+}
+?>
