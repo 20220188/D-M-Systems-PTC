9cf3ef3b0f151ae43ece86ec4d0138319a2817ab
RodrigoDiaz13 <108245842+RodrigoDiaz13@users.noreply.github.com>
2024-06-30 21:58:00 -0600

    Update admin_usuario_handler.php

--- api/models/handler/admin_usuario_handler.php
+++ api/models/handler/admin_usuario_handler.php
@@ -29,7 +29,15 @@ class UsuarioHandler
         $params = array($this->usuario, $this->clave, $this->correo, $this->nombre, $this->DUI, $this->telefono, $this->id_nivel_usuario);
         return Database::executeRow($sql, $params);
     }
-
+    public function searchRows($value)
+    {
+        $sql = 'SELECT id_proveedor, codigo_proveedor, nombre_proveedor, pais_proveedor, giro_negocio_proveedor, dui_proveedor, nombre_comercial_proveedor, fecha_proveedor, nit_proveedor, telefono_proveedor, contacto_proveedor, direccion_proveedor, departamento_proveedor, municipio_proveedor
+                FROM proveedor
+                WHERE nombre_proveedor LIKE ? OR codigo_proveedor LIKE ?
+                ORDER BY nombre_proveedor';
+        $params = array("%$value%", "%$value%");
+        return Database::getRows($sql, $params);
+    }
     public function readAll()
     {
         $sql = 'SELECT id_usuario, usuario, clave, correo, nombre, DUI, telefono, id_nivel_usuario
