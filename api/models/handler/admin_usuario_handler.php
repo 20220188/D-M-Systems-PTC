cd5623f7a13fa9b2013c7d271d3267cc1e938151
Juan2022-a <20220671@ricaldone.edu.sv>
2024-07-02 08:12:37 -0600

    seach usuario por nombre

--- api/models/handler/admin_usuario_handler.php
+++ api/models/handler/admin_usuario_handler.php
@@ -29,13 +29,14 @@ class UsuarioHandler
         $params = array($this->usuario, $this->clave, $this->correo, $this->nombre, $this->DUI, $this->telefono, $this->id_nivel_usuario);
         return Database::executeRow($sql, $params);
     }
-    public function searchRows($value)
+    public function searchRows()
     {
-        $sql = 'SELECT id_proveedor, codigo_proveedor, nombre_proveedor, pais_proveedor, giro_negocio_proveedor, dui_proveedor, nombre_comercial_proveedor, fecha_proveedor, nit_proveedor, telefono_proveedor, contacto_proveedor, direccion_proveedor, departamento_proveedor, municipio_proveedor
-                FROM proveedor
-                WHERE nombre_proveedor LIKE ? OR codigo_proveedor LIKE ?
-                ORDER BY nombre_proveedor';
-        $params = array("%$value%", "%$value%");
+        $value = '%' . Validator::getSearchValue() . '%';
+        $sql = 'SELECT id_usuario, usuario, clave, correo, nombre, DUI, telefono, id_nivel_usuario
+                FROM tb_usuarios
+                WHERE nombre LIKE ? 
+                ORDER BY nombre';
+        $params = array($value);
         return Database::getRows($sql, $params);
     }
     public function readAll()
