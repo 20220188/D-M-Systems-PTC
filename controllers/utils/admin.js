6ad46e4cb74d6e3d436f3f8ba7df13718b16efd4
Juan Pablo Flamenco <20220671@ricaldone.edu.sv>
2024-09-18 11:16:43 -0600

    arreglo de errores pequeños

--- controllers/utils/admin.js
+++ controllers/utils/admin.js
@@ -9,7 +9,7 @@ if (MAIN_TITLE) {
 
 const asideContent = `
     <footer>
-        <p>&copy; 2024 Mi Proyecto. Todos los derechos reservados.</p>
+        <p>&copy; 2024 D-M SYSTEM. Todos los derechos reservados.</p>
     </footer>
 `;
 
@@ -210,41 +210,38 @@ const loadTemplate = async () => {
 //Ejecutar función en el evento click
 document.getElementById("btn_open").addEventListener("click", open_close_menu);
 
-//Declaramos variables
-var side_menu = document.getElementById("menu_side");
-var btn_open = document.getElementById("btn_open");
-var body = document.getElementById("body");
+// Asignar el botón de abrir/cerrar menú y el cuerpo del documento.
+const btnOpen = document.getElementById("btn_open");
+const sideMenu = document.getElementById("menu_side");
+const body = document.getElementById("body");
 
-//Evento para mostrar y ocultar menú
-    function open_close_menu(){
+// Verificar si btnOpen, sideMenu y body existen antes de agregar el evento.
+if (btnOpen && sideMenu && body) {
+    // Evento para mostrar y ocultar menú
+    function open_close_menu() {
         body.classList.toggle("body_move");
-        side_menu.classList.toggle("menu__side_move");
+        sideMenu.classList.toggle("menu__side_move");
     }
 
-//Si el ancho de la página es menor a 760px, ocultará el menú al recargar la página
+    // Asignar el evento click al botón de abrir/cerrar menú
+    btnOpen.addEventListener("click", open_close_menu);
 
-if (window.innerWidth < 760){
-
-    body.classList.add("body_move");
-    side_menu.classList.add("menu__side_move");
-}
-
-//Haciendo el menú responsive(adaptable)
-
-window.addEventListener("resize", function(){
-
-    if (window.innerWidth > 760){
-
-        body.classList.remove("body_move");
-        side_menu.classList.remove("menu__side_move");
-    }
-
-    if (window.innerWidth < 760){
-
+    // Si el ancho de la página es menor a 760px, ocultará el menú al recargar la página
+    if (window.innerWidth < 760) {
         body.classList.add("body_move");
-        side_menu.classList.add("menu__side_move");
+        sideMenu.classList.add("menu__side_move");
     }
 
-});
+    // Haciendo el menú responsive (adaptable)
+    window.addEventListener("resize", function () {
+        if (window.innerWidth > 760) {
+            body.classList.remove("body_move");
+            sideMenu.classList.remove("menu__side_move");
+        } else {
+            body.classList.add("body_move");
+            sideMenu.classList.add("menu__side_move");
+        }
+    });
+}
 
 };
