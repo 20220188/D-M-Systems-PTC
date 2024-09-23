98062cc7ab2a66eaa682c00b873cb772365ee504
20220188 <20220188@ricaldone.edu.sv>
2024-09-23 09:44:00 -0600

    cambios para poder subir la api

--- controllers/utils/components.js
+++ controllers/utils/components.js
@@ -2,7 +2,7 @@
 *   CONTROLADOR DE USO GENERAL EN TODAS LAS PÁGINAS WEB.
 */
 // Constante para establecer la ruta base del servidor.
-const SERVER_URL = 'http://localhost/D-M-SYSTEMS-PTC/api/';
+const SERVER_URL = 'https://34.133.78.15/D-M-SYSTEMS-PTC/api/';
 
 /*
 *   Función para mostrar un mensaje de confirmación. Requiere la librería sweetalert para funcionar.
